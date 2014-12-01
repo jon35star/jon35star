33288c19503d083c4a32be284aa61eb045df4606
j <j@hc>
2014-12-01 21:55:11 +0900

    test

---
+++ test/WebContent/views/board/list.jsp
@@ -0,0 +1,9 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+
+    
+<div>
+	<div><dl><dd></dd><dd></dd><dd></dd></dl></div>
+
+
+</div>
---
+++ test/src/com/hw/HwController.java
@@ -0,0 +1,16 @@
+package com.hw;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HwController {
+
+	@RequestMapping(value="/hw.go")
+	public String hw(){
+		
+		
+		
+		return null;	
+	}
+}
